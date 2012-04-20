d8197921f0cb4e56a5a16db9c2f07aa7a2768663
Chris Channing <christopher.channing@gmail.com>
2012-04-20 09:53:49 -0500

    Adding quadrocore sln

---
+++ xplained/a3bu/quadrocore/src/quadrocore.c
@@ -0,0 +1,16 @@
+/*
+ * quadrocore.c
+ *
+ * Created: 4/20/2012 9:50:45 AM
+ *  Author: cchannin
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
