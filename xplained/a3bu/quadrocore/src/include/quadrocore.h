8e5e359ec7ead65298bd450b8bf5eec23f5ec92a
Chris Channing <christopher.channing@gmail.com>
2012-04-20 10:17:08 -0500

    Fleshing out project settings

---
+++ xplained/a3bu/quadrocore/src/include/quadrocore.h
@@ -0,0 +1,9 @@
+#ifndef QUADROCORE_H_
+#define QUADROCORE_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+
+#endif /* QUADROCORE_H_ */
--- xplained/a3bu/quadrocore/src/quadrocore.c
+++ xplained/a3bu/quadrocore/src/quadrocore.c
@@ -1,16 +1,8 @@
-/*
- * quadrocore.c
- *
- * Created: 4/20/2012 9:50:45 AM
- *  Author: cchannin
- */ 
-
-#include <avr/io.h>
+#include "quadrocore.h"
 
 int main(void)
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	for ( ; ; );
+	
+	return 0;
 }
